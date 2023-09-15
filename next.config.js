e65dc7efe2c80ba639b5403d06a5c6954cd478d4
fulldev1023 <simbadevlop@gmail.com>
2023-09-15 09:15:42 +0200

    chore: next is served at /app

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   publicRuntimeConfig: {
     legacyAppUrl: process.env.NEXT_PUBLIC_LEGACY_APP_URL,
   },
