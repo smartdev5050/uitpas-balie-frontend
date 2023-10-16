a57aa99f1cef65f69218cf2833449ddb33af521e
fulldev1023 <simbadevlop@gmail.com>
2023-10-16 15:25:08 +0200

    fix: localstorage is not known in Java browser
    Refs #UPS-4680

--- src/feature-counter/context/counterStore.ts
+++ src/feature-counter/context/counterStore.ts
@@ -3,13 +3,18 @@ import { Counter } from "@/feature-counter/context/CounterContext";
 const STORAGE_KEY = "@uitpas-balie/counter";
 
 export const storeCounter = (counter: Counter) => {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(counter));
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(counter));
+  } catch (e) {
+    console.debug("Could not set localStorage item", e);
+  }
 };
 
 export const readCounter = (): Counter => {
   try {
     return JSON.parse(localStorage.getItem(STORAGE_KEY) ?? "") ?? null;
-  } catch {
+  } catch (e) {
+    console.debug("Could not parse localStorage item", e);
     return null;
   }
 };
