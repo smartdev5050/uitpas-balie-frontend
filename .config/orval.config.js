188b0f252356a3b5f11c1bfca37483cefb81da46
fulldev1023 <simbadevlop@gmail.com>
2023-10-23 17:24:01 +0200

    feat: add second configuration for search api

--- .config/orval.config.js
+++ .config/orval.config.js
@@ -6,8 +6,21 @@ module.exports = {
     },
     output: {
       mode: "tags-split",
-      target: "../src/lib/dataAccess/generated/uitpas.ts",
-      schemas: "../src/lib/dataAccess/generated/model",
+      target: "../src/lib/dataAccess/uitpas/generated/uitpas.ts",
+      schemas: "../src/lib/dataAccess/uitpas/generated/model",
+      client: "react-query",
+      mock: false,
+    },
+  },
+  search: {
+    input: {
+      target:
+        "https://stoplight.io/api/v1/projects/publiq/uitdatabank/nodes/reference/search.json?deref=optimizedBundle",
+    },
+    output: {
+      mode: "tags-split",
+      target: "../src/lib/dataAccess/search/generated/search.ts",
+      schemas: "../src/lib/dataAccess/search/generated/model",
       client: "react-query",
       mock: false,
     },
