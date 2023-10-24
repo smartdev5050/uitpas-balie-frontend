63d1e9663e5e650226bb6683e232a74d57a9dce5
fulldev1023 <simbadevlop@gmail.com>
2023-10-24 17:17:38 +0200

    fix: storing counter data via cookies

--- src/feature-counter/context/counterStore.ts
+++ src/feature-counter/context/counterStore.ts
@@ -1,23 +1,21 @@
-import { storeData, readData } from "@/lib/utils/localStorageUtils";
+import { readCookie, storeCookie } from "@/lib/utils/cookieUtils";
 import { Counter } from "@/feature-counter/context/CounterContext";
 
-// TODO: find an alternative way of reading/storing counter & prev counter
-// as JavaFX browser does not support localStorage
 const COUNTER_STORAGE_KEY = "@uitpas-balie/counter";
 const PREV_COUNTER_STORAGE_KEY = "@uitpas-balie/counter-prev";
 
 export const storeCounter = (counter: Counter) => {
-  storeData(COUNTER_STORAGE_KEY, counter);
+  storeCookie(COUNTER_STORAGE_KEY, counter);
 };
 
 export const storePrevCounter = (counter: Counter) => {
-  storeData(PREV_COUNTER_STORAGE_KEY, counter);
+  storeCookie(PREV_COUNTER_STORAGE_KEY, counter);
 };
 
 export const readCounter = (): Counter => {
-  return readData(COUNTER_STORAGE_KEY);
+  return readCookie(COUNTER_STORAGE_KEY);
 };
 
 export const readPrevCounter = (): Counter => {
-  return readData(PREV_COUNTER_STORAGE_KEY);
+  return readCookie(PREV_COUNTER_STORAGE_KEY);
 };
