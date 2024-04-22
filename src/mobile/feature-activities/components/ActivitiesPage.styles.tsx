278d4679c15cc4446eb330acede67147a7d46a58
fulldev1023 <simbadevlop@gmail.com>
2024-04-22 13:57:28 +0200

    feat: visualize scrollable activity container

--- src/mobile/feature-activities/components/ActivitiesPage.styles.tsx
+++ src/mobile/feature-activities/components/ActivitiesPage.styles.tsx
@@ -1,13 +1,24 @@
 import { Stack, styled } from "@mui/material";
 
-export const ScrollableActivitesContainer = styled(Stack)({
-  height: "380px",
-  overflowY: "auto",
+export const ScrollableActivitesContainer = styled(Stack)(({ theme }) => ({
+  height: "410px",
+  overflowY: "scroll",
   "::-webkit-scrollbar": {
-    display: "none",
+    width: "6px",
+    color: theme.palette.primary.main,
   },
-  scrollbarWidth: "none",
+  "::-webkit-scrollbar-button": {
+    background: "transparent",
+  },
+  "::-webkit-scrollbar-track-piece": {
+    background: "transparent",
+  },
+  "::-webkit-scrollbar-thumb": {
+    background: theme.palette.primary.main,
+    borderRadius: "10px",
+  },
   msOverflowStyle: "none",
   mb: 3,
+  paddingRight: "8px",
   gap: "12px",
-});
+}));
--- src/mobile/feature-activities/components/ActivitiesPicker.tsx
+++ src/mobile/feature-activities/components/ActivitiesPicker.tsx
@@ -1,13 +1,22 @@
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Stack } from "@mui/material";
 import { Search } from "@/shared/lib/dataAccess";
 import { OutlinedButton } from "@/mobile/lib/ui/uitpas/OutlinedButton";
 import { ScrollableActivitesContainer } from "@/mobile/feature-activities/components/ActivitiesPage.styles";
-import { Dispatch, SetStateAction, UIEvent, useEffect, useRef } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  UIEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { useTranslation } from "@/shared/lib/i18n/client";
 import { Typography } from "@/mobile/lib/ui";
 import { useSearchQuery } from "@/shared/lib/utils/hooks/useSearchQuery";
 import { useActivity } from "@/mobile/feature-activities/context/useActivity";
 import { useRouter } from "next/navigation";
+import { ExpandMore } from "@mui/icons-material";
+import Icon from "@mdi/react";
 
 type ActivitiesPickerProps = {
   isInitialLoading: boolean;
@@ -37,28 +46,49 @@ export const ActivitiesPicker = ({
   const scrollRef = useRef<HTMLDivElement>(null);
   const { searchQuery } = useSearchQuery();
   const { setSelectedActivity } = useActivity();
+  const [hasMoreItems, setHasMoreItems] = useState<boolean>(false);
 
   useEffect(() => {
     if (scrollRef.current && data.member.length > fetchLimit) {
-      scrollRef.current.scrollTop =
-        scrollPosition + (data.member.length > 7 ? 56 : 0);
+      // Scroll one (new) item down
+      scrollRef.current.scrollTo({
+        top: scrollPosition + (data.member.length > 7 ? 56 : 0),
+        behavior: "smooth",
+      });
     }
   }, [data.member]);
 
   const LANG_KEY = i18n.language as keyof Search.EventName;
 
+  useEffect(() => {
+    if (totalFetchedItems === 0) return;
+    setHasMoreItems(totalFetchedItems > data.member.length);
+  }, [data.member.length, totalFetchedItems]);
+
   const handleScroll = (e: UIEvent<HTMLDivElement>) => {
     const bottom =
-      e.currentTarget.scrollHeight - e.currentTarget.scrollTop ===
+      Math.trunc(e.currentTarget.scrollHeight - e.currentTarget.scrollTop) <=
       e.currentTarget.clientHeight;
-    if (!bottom) return;
 
-    const hasMoreItems = totalFetchedItems > data.member.length;
-    if (!hasMoreItems) return;
+    if (!bottom || !hasMoreItems) return;
 
     setOffset((prev) => prev + fetchLimit);
     setScrollPosition(e.currentTarget.scrollTop);
+    scrollRef.current?.scrollTo({
+      top: e.currentTarget.scrollTop,
+      behavior: "smooth",
+    });
   };
+
+  useEffect(() => {
+    // Ensure that we're showing the loading spinner when fetching
+    if (isFetching) {
+      scrollRef.current?.scrollTo({
+        top: scrollPosition + 100, // 100 -> doesn't have to be exact, it will bottom out anyway
+        behavior: "smooth",
+      });
+    }
+  }, [isFetching]);
 
   const handleActivityClick = (activity: Search.Event) => {
     setSelectedActivity(activity);
@@ -71,37 +101,39 @@ export const ActivitiesPicker = ({
 
   if (data.totalItems > 0) {
     return (
-      <ScrollableActivitesContainer ref={scrollRef} onScroll={handleScroll}>
-        {data.member.map((activity) => (
-          <OutlinedButton
-            onClick={() => handleActivityClick(activity)}
-            key={activity["@id"]}
-            sx={{
-              ...(activity.isNew
-                ? {
-                    opacity: 0,
-                    transform: "translateY(20px)",
-                    animation: "fade-in 0.3s ease-out forwards",
-                    "@keyframes fade-in": {
-                      "0%": {
-                        opacity: 0,
-                        transform: "translateY(20px)",
+      <Stack>
+        <ScrollableActivitesContainer ref={scrollRef} onScroll={handleScroll}>
+          {data.member.map((activity) => (
+            <OutlinedButton
+              onClick={() => handleActivityClick(activity)}
+              key={activity["@id"]}
+              sx={{
+                ...(activity.isNew
+                  ? {
+                      opacity: 0,
+                      transform: "translateY(20px)",
+                      animation: "fade-in 0.3s ease-out forwards",
+                      "@keyframes fade-in": {
+                        "0%": {
+                          opacity: 0,
+                          transform: "translateY(20px)",
+                        },
+                        "100%": {
+                          opacity: 1,
+                          transform: "translateY(0)",
+                        },
                       },
-                      "100%": {
-                        opacity: 1,
-                        transform: "translateY(0)",
-                      },
-                    },
-                  }
-                : {}),
-            }}
-          >
-            {activity.name[LANG_KEY]}
-          </OutlinedButton>
-        ))}
+                    }
+                  : {}),
+              }}
+            >
+              {activity.name[LANG_KEY]}
+            </OutlinedButton>
+          ))}
 
-        {isFetching && <CircularProgress sx={{ m: "auto auto" }} size={20} />}
-      </ScrollableActivitesContainer>
+          {isFetching && <CircularProgress sx={{ m: "auto auto" }} size={20} />}
+        </ScrollableActivitesContainer>
+      </Stack>
     );
   }
 
